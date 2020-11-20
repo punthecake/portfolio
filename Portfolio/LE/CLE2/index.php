3511f898f92b8be4175ea9f373eca832fe0f754e
punthecake <60122576+punthecake@users.noreply.github.com>
2020-11-20 18:30:25 +0800

    Add files via upload

---
+++ Portfolio/LE/CLE2/index.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>Jubelag_CLE2</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+            <object type="image/svg+xml" data="background.svg" class="background"></object>
+            <div class="content">
+                <div class="wind-1">
+                    <svg height=800 width="800">
+                        <style>
+                        @keyframes dash {
+                        100% {
+                            stroke-dashoffset: 0;
+                        }
+                        }</style>
+                        <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 300 L 300 300 A 50 50 0 1 0 250 250 "/>
+                        <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 450 L 650 450 A 50 50 329 1 0 600 400"/>
+                        <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 650 L 300 650 A 50 50 0 1 1 250 700"/>
+                        <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 600 L 550 600 A 50 50 0 1 1 500 650 "/>
+                        <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 350 L 550 350 A 50 50 0 1 0 500 300 "/>
+                       </svg>
+                </div>
+                <div class="content">
+                    <div class="wind-2">
+                        <svg height=800 width="800">
+                            <style>
+                            @keyframes dash {
+                            100% {
+                                stroke-dashoffset: 0;
+                            }
+                            }</style>
+                            <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 300 L 300 300 A 50 50 0 1 0 250 250 "/>
+                            <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 450 L 650 450 A 50 50 329 1 0 600 400"/>
+                            <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 650 L 300 650 A 50 50 0 1 1 250 700"/>
+                            <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 600 L 550 600 A 50 50 0 1 1 500 650 "/>
+                            <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear infinite;" fill="none" stroke-width="10" stroke="white" opacity="0.7" d="M 0 350 L 550 350 A 50 50 0 1 0 500 300 "/>
+                           </svg>
+                    </div>
+                    <div class="banner">
+                    <div class="wood">
+                        <svg height=800 width="800">
+                            <path fill="#996633" d="M 100 0 L 100 400 L 150 400 L 150 0 L 100 0 "/>
+                            <path fill="#996633" d="M 650 0 L 650 400 L 700 400 L 700 0 L 650 0"/>
+                            <path fill="#ccaa66" d="M 0 400 L 800 400 L 800 800 L 0 800 L 0 400 "/>
+                        </svg>
+                    <h1>FLOOM</h1>
+                </div>
+                    </div>
+            </div>
+            <div class="footer">
+                <object type="image/svg+xml" data="garden.svg" class="garden"></object>
+        </div>
+    </body>
+</html>
