b385b1b30bcc675d361f5b8b901bf82637fd5f3d
punthecake <60122576+punthecake@users.noreply.github.com>
2020-12-18 20:24:03 +0800

    Add files via upload

---
+++ Portfolio/home.php
@@ -0,0 +1,167 @@
+<html>
+    <head>
+        <title> Home | Portfolio </title>
+        <link rel="stylesheet" type="text/css" href="css/stylesss.css">
+    </head>
+    <body>
+        <div class="wrap">
+        <div class="head">
+            <div class="main">
+            <span class="webdev">Pan's Portfolio</span>
+            <span class="socod">CSS, SVG, Canvas</span>
+            </div>
+            </div>
+            
+        <div class="main-cont">
+        <div class="left-content">
+        <div class="left-wrap">
+                <svg height=300 width="1380">
+                    <style>
+                        @keyframes dash {100% { stroke-dashoffset: 0;}}
+                    </style>
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d="M 98 54 L 100 250  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d="M 100 150 L 200 150  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d="M 200 50 L 200 250  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="30" stroke="#1aa6b7" d="M 300 50 L 300 250  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d=" M 300 238 L 400 238 "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d=" M 300 150 L 400 150  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d=" M 300 63 L 400 62"/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 5s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d="M 500 50 L 500 250 L 500 250 L 600 250"/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 5s linear forwards;" fill="none" stroke-width="25" stroke="#1aa6b7" d="M 700 50 L 700 250 L 700 250 L 800 250  "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 3s linear forwards;" fill="none" stroke-width="30" stroke="#1aa6b7" d="M  950 250 A 50 50 0 1 1 950 50 A 50 50 0 1 1 950 250 "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 5s linear forwards;" fill="none" stroke-width="30" stroke="#1aa6b7" d="M  1200 150 L 1150 50 L 1250 50 L 1200 150 "/>
+    
+                    <path style="stroke-dasharray: 1500; stroke-dashoffset: 1500; animation: dash 10s linear forwards;" fill="none" stroke-width="20" stroke="#1aa6b7" d="M 1200 200 A 33 27 0 1 1 1201 200 "/>
+    
+                </svg>
+            </div>
+        </div>
+        <div class="right-content">
+            <h1>WELCOME TO MY WEBSITE!</h1>
+            <p>This website is intended to be a portfolio for my web designs and animations! Here, you can explore some of my proud artworks and creations. My animations and designs are mix of CSS, SVG, and HTML 5 Canvas. Hope you'll visiting my website!</p></div>
+        </div>
+        <div class="main-cont">
+        <div class="left-content">
+            <h1>ABOUT THE AUTHOR</h1>
+            <p> Hello everyone. My name is Francisco Jubelag Jr. XV. I am a third year Information Technology student from University of St. La Salle. I am an aspiring Web designer and developer. I like to create web animations and designs. I am still studying for ways to improve my creativity and hoping to be a successful developer someday. </p></div>
+        <div class="right-content">
+            <div class="right-wrap">
+            <object type="image/svg+xml" data="svg/avatar.svg" class="author"></object></div>
+            </div>
+        </div>
+        <div class="main-cont">
+        <div class="left-content">
+            <div class="slider">   
+            <a href="#slide-1">1</a>
+            <a href="#slide-2">2</a>
+            <a href="#slide-3">3</a>
+            <a href="#slide-4">4</a>
+            <a href="#slide-5">5</a>
+            <div class="slides">
+                <div id="slide-1">
+                <canvas id="vaporwave" width="200" height="200"></canvas>
+                </div>
+                <div id="slide-2">
+                <canvas id="netflix" width="200" height="200"></canvas>
+                </div>
+                <div id="slide-3">
+                    <div class="ic-contain">
+                    <div class="icecream"></div>
+                    <div class="popsicle"></div>
+                    <div class="ic-detail1"></div>
+                    <div class="ic-detail2"></div>
+                    <div class="ic-melt1"></div>
+                    <div class="ic-melt2"></div>
+                    </div>
+                </div>
+                <div id="slide-4">
+                    <div class="tea-container">
+                    <div class="cup"></div>
+                    <div class="milk"></div>
+                    <div class="straw"></div>
+                    <div class="pearl1"></div>
+                    <div class="pearl2"></div>
+                    <div class="pearl3"></div>
+                    </div>
+                </div>
+                <div id="slide-5">
+                        <svg height="300" width="300">
+                        <rect height="300" width="300" fill="#8a95b0"/>
+                        <path fill="#a5b4d4" d="M 0 300 L 0 250 Q 150 150 300 250 L 300 300 Z"/>
+                        <path fill="#a5b4d4" d="M 150 50 L 100 125 L 200 125 L 150 50"/>
+                        <path fill="#a5b4d4" d="M 150 100 L 100 175 L 200 175 L 150 100"/>
+                        <path fill="#a5b4d4" d="M 150 150 L 90 225 L 210 225 L 150 150"/>
+                        
+                        <!-- snow -->
+                        <circle cx="10" cy="0" r="5" fill="white">
+                        <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                        dur="1.5s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="30" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                            dur="1.7s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="50" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.3s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="90" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.6s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="120" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.8s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="150" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.4s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="180" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.9s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="220" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="2.3s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="235" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.7s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="260" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="2s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="280" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="1.9s" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="300" cy="0" r="5" fill="white">
+                            <animate attributeType="XML" attributeName="cy" from="0" to="300"
+                                dur="2.2s" repeatCount="indefinite"/>
+                        </circle>
+                        </svg>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="right-content">
+        <h1>ARTWORK SHOWCASE</h1>
+        <p>Here are some samples of my designs and animations. You can see more of them from the link above.</p></div>
+        </div>
+        </div>
+    </body>
+</html>
+<script type="text/javascript" src="js/vaporwave.js"></script>
+<script type="text/javascript" src="js/netflix.js"></script>
